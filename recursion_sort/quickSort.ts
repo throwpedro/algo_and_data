2a70226eac9e5a79be881d4e7daa9191cb220352
Thor Döör <thordoor89@hotmail.com>
2024-01-13 21:09:07 +0100

    at()

--- recursion_sort/quickSort.ts
+++ recursion_sort/quickSort.ts
@@ -2,7 +2,7 @@ export const quickSort = (nums: number[]): number[] => {
     if (nums.length < 2) {
         return nums;
     }
-    const pivot = nums[nums.length - 1];
+    const pivot = nums.at(-1)!;
     const left = [];
     const right = [];
     for (let i = 0; i < nums.length - 1; i++) {
